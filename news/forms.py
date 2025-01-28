882729548dd8da45a6c8105baa5612391c1c2f8f
ribondareva <msbondareva01@gmail.com>
2025-01-28 20:24:23 +0300

    Created form to add comment

--- news/forms.py
+++ news/forms.py
@@ -1,14 +1,13 @@
 from django import forms
 from django.core.exceptions import ValidationError
-from .models import Post, Category
+from .models import Post, Category, Comment
 
 
 class PostForm(forms.ModelForm):
 
     class Meta:
         model = Post
-        fields = ['author', 'title', 'category', 'text']
-
+        fields = ["author", "title", "category", "text"]
 
     def clean(self):
         cleaned_data = super().clean()
@@ -16,15 +15,18 @@ class PostForm(forms.ModelForm):
         title = cleaned_data.get("title")
 
         if title == text:
-            raise ValidationError(
-                "Описание не должно быть идентично названию."
-            )
+            raise ValidationError("Описание не должно быть идентично названию.")
 
         return cleaned_data
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ["text"]
+
 
 class SearchForm(forms.ModelForm):
     title = forms.CharField(required=False)
     category = forms.ModelChoiceField(queryset=Category.objects.all(), required=False)
     creationDate = forms.DateField(required=False)
-
-
